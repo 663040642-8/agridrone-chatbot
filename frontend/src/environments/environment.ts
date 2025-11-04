5aadbbf8da4f9e90b38320c0ffbfe78c82172240
Kittikan <kittikan.pi@kkumail.com>
2025-11-04 20:26:35 +0700

    Add apiUrl for production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: '',
+    apiUrl: 'https://agridrone-chatbot.onrender.com',
     supabaseUrl: 'https://vwhctqklbvdvqpljgwzi.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3aGN0cWtsYnZkdnFwbGpnd3ppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMTI0NzEsImV4cCI6MjA3NzY4ODQ3MX0.wN1bAhq36wsCrucaxuhhlg5yLcK7NTbefIJaClhBBkk',
 };
